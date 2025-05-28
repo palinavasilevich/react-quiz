b160ac79cac8377a7071e26dec11a8938beaeb2c
pvslvch <polina.vasilevichh@gmail.com>
2025-05-28 12:28:43 +0200

    feat: implement the logic of shuffling answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,10 +2,13 @@ import { useState } from "react";
 
 import QUESTIONS from "../questions";
 
+import quizCompleteImg from "../assets/quiz-complete.png";
+
 export const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   const handleSelectAnswer = (selectedAnswer) => {
     setUserAnswers((prevUserAnswers) => {
@@ -13,12 +16,24 @@ export const Quiz = () => {
     });
   };
 
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Quiz Complete Image" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer, index) => (
+          {shuffledAnswers.map((answer, index) => (
             <li key={index} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
