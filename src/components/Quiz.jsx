ed80c8d598342e0907031eb5e852be63e438904f
pvslvch <polina.vasilevichh@gmail.com>
2025-05-28 15:48:23 +0200

    feat: implement Quiz Summary component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,9 @@
 import { useCallback, useState } from "react";
 
-import QUESTIONS from "../questions";
-
-import quizCompleteImg from "../assets/quiz-complete.png";
 import { Question } from "./Question";
+import { Summary } from "./Summary";
+
+import QUESTIONS from "../questions";
 
 export const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -23,12 +23,7 @@ export const Quiz = () => {
   );
 
   if (quizIsComplete) {
-    return (
-      <div id="summary">
-        <img src={quizCompleteImg} alt="Quiz Complete Image" />
-        <h2>Quiz Completed!</h2>
-      </div>
-    );
+    return <Summary userAnswers={userAnswers} />;
   }
 
   return (
